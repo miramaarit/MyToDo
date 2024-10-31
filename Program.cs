924e2d47ae88afd472de2cadb119bda77d2202fd
Mira Karjalainen <mira.karjalainen@consid.se>
2024-10-31 14:51:49 +0100

    Flytta projektfiler till huvudmappen och uppdatera .gitignore för att ignorera byggfiler

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static List<string> toDoList = new List<string>();
+
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+            Console.WriteLine("Todo-lista:");
+            for (int i = 0; i < toDoList.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {toDoList[i]}");
+            }
+            Console.WriteLine("\nVad vill du göra?");
+                Console.WriteLine("1. Lägg till uppgift");
+                Console.WriteLine("2. Ta bort uppgift");
+                Console.WriteLine("3. Avsluta");
+            
+        }
+    }
+}
