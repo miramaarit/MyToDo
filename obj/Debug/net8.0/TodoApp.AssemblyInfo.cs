1f7b9b4fc0037aeae4d59ed6a641681aea5c862e
Mira Karjalainen <mira.karjalainen@consid.se>
2024-11-01 15:24:04 +0100

    something

--- obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f9ac3eebeb5b38fbd89460471335b960791db5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b6a75279647230b1f4097eafa9cb6ce9a9eacf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
